41f0382b3253988f2df5f9e82b2cb31b9b99e0fe
Administrator <1027050822@qq.com>
2020-08-17 21:04:06 +0800

    1.新增三种押题卷的第三套 2.新增了正确答案的展示

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,6 +6,8 @@
       <p style="margin-bottom: 0;text-align: left;font-size: medium;font-weight: bold"
          v-html="(key+1)+'. '+item.question"></p>
       <p style="margin-bottom: 1px;margin-top: 1px;text-align: left; font-size: small" v-for="(answer,i) in item.answers" v-bind:key="i" v-html="letters[i]+'.'+answer"></p>
+      <p style="margin-bottom: 0;text-align: left;font-size: small;font-weight: bold"> 正确答案:</p>
+      <p style="margin-bottom: 0;text-align: left;font-size: small; " v-html="item.realAnswer.toString()"></p>
       <p style="margin-bottom: 0;text-align: left;font-size: small;font-weight: bold"> 解析:</p>
       <p style="margin-bottom: 0;text-align: left;font-size: small; " v-html="item.analyticDesc"></p>
     </div>
@@ -13,21 +15,36 @@
 </template>
 
 <script>
-import exam from '../../static/2020年《经济法》押题卷（二）'
+import exam from '../../static/2020年《经济法》押题卷（三）'
 
 export default {
   name: 'Home',
   mounted: function () {
     this.questionsDetail = exam.Topic
     this.answersDetail = exam.Answer
+    // 遍历所有题目
     for (let i of this.questionsDetail) {
       let object = Object()
       object.question = i.Topic
       object.answers = []
       object.analyticDesc = i.AnalyticDesc
+      object.realAnswer = []
+      // 拿到正确答案,如果多选,按逗号分开
+      let answerIds = i.AnswersIds.split(',')
+      let answerEntities = []
+      // 循环所有答案,匹配当前题目的questionId
       for (let j of this.answersDetail) {
         if (j.QuestionId === i.QuestionId) {
           object.answers.push(j.AnswerContent)
+          answerEntities.push(j)
+        }
+      }
+      // 遍历正确答案,从待选答案中匹配出下标,再从letters数组中取选项
+      for (let l of answerIds) {
+        for (let k = 0; k < answerEntities.length; k++) {
+          if (answerEntities[k].AnswerID === l) {
+            object.realAnswer.push(this.letters[k])
+          }
         }
       }
       this.showData.push(object)
